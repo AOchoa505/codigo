f0229377c35be8fffb2a17e0bb38db33b8440c67
AOchoa505 <0501200705902@iteesa.edu.hn>
2024-09-13 11:18:56 -0600

    feat: Nuevo Controlador

---
+++ app/Http/Controllers/login/frmRegistroCtrl.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\login;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\usuario\usuarioCtrl;
+class frmRegistroCtrl extends Controller
+{
+    public function mostrarDatos(){
+        return view ("registro.frmRegistro");
+        
+
+    }
+}
